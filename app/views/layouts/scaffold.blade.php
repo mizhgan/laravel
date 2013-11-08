f4818a7ee65cd80d271b5f90757b2eba9317bc7d
mizhgan <i@mizhgan.ru>
2013-11-08 12:34:43 +0400

    Add social services share buttons to all pages

--- app/views/layouts/scaffold.blade.php
+++ app/views/layouts/scaffold.blade.php
@@ -24,6 +24,8 @@
 	</head>
 
 	<body>
+		
+		<div style="position: fixed; top: 150px; left: 5px; z-index: 1000;" class="pluso" data-background="transparent" data-options="medium,square,line,vertical,counter,theme=06" data-services="vkontakte,odnoklassniki,facebook,twitter,google,moimir,email,print"></div>
 
 		<div id="wrap">
 
@@ -133,6 +135,16 @@
 				$.jGrowl("<p>{{ Session::get('message') }}</p>");
 	      	</script>
 	    @endif
+	    <script type="text/javascript">(function() {
+		  if (window.pluso)if (typeof window.pluso.start == "function") return;
+		  if (window.ifpluso==undefined) { window.ifpluso = 1;
+		    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+		    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+		    s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+		    var h=d[g]('body')[0];
+		    h.appendChild(s);
+		  }})();
+		</script>
 
 	</body>
 
